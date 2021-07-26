823081ad33461488a9bbdda0de08b8f49f97cbff
Cecilia Nunn <cecilia.nunn@gmail.com>
2021-07-26 20:17:24 +0100

    index cards working

--- power-dressing-app/client/src/components/Outfits/OutfitCard.js
+++ power-dressing-app/client/src/components/Outfits/OutfitCard.js
@@ -1,30 +1,22 @@
 import React from 'react'
-import Nav from 'react-bootstrap/Nav'
-import Col from 'react-bootstrap/Col'
+import { Card, Button } from 'react-bootstrap'
 
 
 const OutfitCard = ({ _id, title, image }) => {
 
   return (
-    <Nav.Item >
-      <Col className='indexShow' xs={12} style={{
-        minHeight: '300px',
-        backgroundImage: `url("${image}")`,
-        backgroundSize: 'cover',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-        padding: '10px',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        color: 'white',
-
-      }}>
-        <span id='cardName'><Nav.Link href={`http://localhost:3000/outfits/${_id}`}>{title}</Nav.Link></span>
-      </Col>
-    </Nav.Item>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img className = "card-image" variant="top" img src={`${image}`}/>
+      <Card.Body id = {_id}>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+          {/* {quote} */}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   )
 }
+
 
 export default OutfitCard
