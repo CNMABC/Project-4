469de41dcd284e30b7a72df958f7b39386aee10c
Cecilia Nunn <cecilia.nunn@gmail.com>
2021-07-27 16:17:09 +0100

    trialling basket code

--- power-dressing-app/client/src/components/Basket/Basket.js
+++ power-dressing-app/client/src/components/Basket/Basket.js
@@ -1,10 +1,29 @@
-// import React from 'react'
-// import { useHistory } from 'react-router-dom'
+import React, {useEffect, useState } from 'react'
+import axios from 'axios'
 
-// const Basket = () => {
 
-//   return (
-//     <h1>Checkout</h1>
-//   )
-// }
-// export default Basket 
+const Basket = () => {
+
+const [getOutfit, setOutfit] = useState([])
+
+const getItem = () => {
+  const item = localStorage.getItem('_id')
+  return JSON.parse.apply(item)
+}
+
+useEffect(() => {
+  console.log('getItem--->',getItem())
+  const getData = asysnc () => {
+    const { data } = await axios.get(`/api/outfits/${id}`)
+
+  }
+})
+
+
+
+
+  return (
+    <h1>Checkout</h1>
+  )
+}
+export default Basket 
